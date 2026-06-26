faa8ec94bcf8bea38a24bd484d26c3497f9ea0c4
isoryndefi <wdbaga@gmail.com>
2026-06-26 15:11:06 +0700

    Create route.ts

---
+++ api/markets/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json([
+    {
+      id: 1,
+      title: "BTC > $150K",
+      probability: 63.4
+    }
+  ]);
+}
