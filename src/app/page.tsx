d0033d8a4232610895ec9f61ee8dad70713730a5
isoryndefi <wdbaga@gmail.com>
2026-06-26 15:09:30 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import MarketCard from "@/components/MarketCard";
+import Analytics from "@/components/Analytics";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <MarketCard />
+      <Analytics />
+    </>
+  );
+}
